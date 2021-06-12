80f8b2c08ffe3f4aa441bd3feafcaa973d9d139a
nulysses <adr.zivil@gmail.com>
2021-06-12 11:04:00 +0200

    1

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ cd "${script_dir}/build"
 git clone https://github.com/sickill/stderred.git
 cd "${script_dir}/build/stderred"
 
-pacman -Syu
+pacman -Syu --noconfirm
 pacman -Sy base-devel cmake --noconfirm
 
 make
