df1cda4069aa57a16cf93976ad2ed2b25e1a823a
nulysses <adr.zivil@gmail.com>
2021-07-10 11:33:16 +0200

    1

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 # Prepare script environment
 {
-  # Script template version 2021-07-10_11:24:30
+  # Script template version 2021-07-10_11:32:20
   # Get old shell option values to restore later
   shopt -s inherit_errexit
   IFS=$'\n' shell_options=($(shopt -op))
@@ -90,11 +90,17 @@ ar18.script.execute_with_sudo cp "${build_dir}/stderred/build/libtest_stderred.s
     eval "${option}"
   done
 }
+function clean_up(){
+  rm -rf "/tmp/${ar18_parent_process}"
+}
 # Return or exit depending on whether the script was sourced or not
 {
   if [ "${ar18_sourced_map["${script_path}"]}" = "1" ]; then
     return "${ar18_exit_map["${script_path}"]}"
   else
+    if [ "${ar18_parent_process}" = "$$" ]; then
+      clean_up
+    fi
     exit "${ar18_exit_map["${script_path}"]}"
   fi
 }
