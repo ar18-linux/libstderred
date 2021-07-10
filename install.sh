68aed786b1461348a425a16d2f3cd808dbcf93c2
nulysses <adr.zivil@gmail.com>
2021-07-10 14:44:27 +0200

    1

--- install.sh
+++ install.sh
@@ -3,13 +3,15 @@
 
 # Prepare script environment
 {
-  # Script template version 2021-07-10_14:27:20
+  # Script template version 2021-07-10_14:41:36
+  script_dir_temp="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+  script_path_temp="${script_dir_temp}/$(basename "${0}")"
   # Get old shell option values to restore later
   if [ ! -v ar18_old_shopt_map ]; then
     declare -A -g ar18_old_shopt_map
   fi
   shopt -s inherit_errexit
-  ar18_old_shopt_map["$(realpath "${BASH_SOURCE[0]}")"]="$(shopt -op)"
+  ar18_old_shopt_map["${script_path_temp}"]="$(shopt -op)"
   set +x
   # Set shell options for this script
   set -o pipefail
@@ -21,7 +23,7 @@
   if [ ! -v LD_PRELOAD ]; then
     LD_PRELOAD=""
   fi
-  ar18_old_ld_preload_map["$(realpath "${BASH_SOURCE[0]}")"]="${LD_PRELOAD}"
+  ar18_old_ld_preload_map["${script_path_temp}"]="${LD_PRELOAD}"
   LD_PRELOAD=""
   # Save old script_dir variable
   if [ ! -v ar18_old_script_dir_map ]; then
@@ -29,9 +31,9 @@
   fi
   set +u
   if [ ! -v script_dir ]; then
-    script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+    script_dir="${script_dir_temp}"
   fi
-  ar18_old_script_dir_map["$(realpath "${BASH_SOURCE[0]}")"]="${script_dir}"
+  ar18_old_script_dir_map["${script_path_temp}"]="${script_dir}"
   set -u
   # Save old script_path variable
   if [ ! -v ar18_old_script_path_map ]; then
@@ -39,13 +41,13 @@
   fi
   set +u
   if [ ! -v script_path ]; then
-    script_path="${script_dir}/$(basename "${0}")"
+    script_path="${script_path_temp}"
   fi
-  ar18_old_script_path_map["$(realpath "${BASH_SOURCE[0]}")"]="${script_path}"
+  ar18_old_script_path_map["${script_path_temp}"]="${script_path}"
   set -u
   # Determine the full path of the directory this script is in
-  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-  script_path="${script_dir}/$(basename "${0}")"
+  script_dir="${script_dir_temp}"
+  script_path="${script_path_temp}"
   #Set PS4 for easier debugging
   export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
   # Determine if this script was sourced or is the parent script
@@ -53,20 +55,20 @@
     declare -A -g ar18_sourced_map
   fi
   if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-    ar18_sourced_map["${script_path}"]=1
+    ar18_sourced_map["${script_path_temp}"]=1
   else
-    ar18_sourced_map["${script_path}"]=0
+    ar18_sourced_map["${script_path_temp}"]=0
   fi
   # Initialise exit code
   if [ ! -v ar18_exit_map ]; then
     declare -A -g ar18_exit_map
   fi
-  ar18_exit_map["${script_path}"]=0
+  ar18_exit_map["${script_path_temp}"]=0
   # Save PWD
   if [ ! -v ar18_pwd_map ]; then
     declare -A -g ar18_pwd_map
   fi
-  ar18_pwd_map["$(realpath "${BASH_SOURCE[0]}")"]="${PWD}"
+  ar18_pwd_map["${script_path_temp}"]="${PWD}"
   if [ ! -v ar18_parent_process ]; then
     export ar18_parent_process="$$"
   fi
@@ -75,7 +77,7 @@
     mkdir -p "/tmp/${ar18_parent_process}"
     cd "/tmp/${ar18_parent_process}"
     curl -O https://raw.githubusercontent.com/ar18-linux/ar18_lib_bash/master/ar18_lib_bash/script/import.sh > /dev/null 2>&1 && . "/tmp/${ar18_parent_process}/import.sh"
-    cd "${ar18_pwd_map["$(realpath "${BASH_SOURCE[0]}")"]}"
+    cd "${ar18_pwd_map["${script_path_temp}"]}"
   fi
 }
 #################################SCRIPT_START##################################
@@ -111,16 +113,16 @@ function clean_up(){
 }
 # Restore environment
 {
+  # Restore PWD
+  cd "${ar18_pwd_map["${script_path}"]}"
   exit_script_path="${script_path}"
   # Restore script_dir and script_path
-  script_dir="${ar18_old_script_dir_map["$(realpath "${BASH_SOURCE[0]}")"]}"
-  script_path="${ar18_old_script_path_map["$(realpath "${BASH_SOURCE[0]}")"]}"
+  script_dir="${ar18_old_script_dir_map["${exit_script_path}"]}"
+  script_path="${ar18_old_script_path_map["${exit_script_path}"]}"
   # Restore LD_PRELOAD
-  LD_PRELOAD="${ar18_old_ld_preload_map["$(realpath "${BASH_SOURCE[0]}")"]}"
-  # Restore PWD
-  cd "${ar18_pwd_map["$(realpath "${BASH_SOURCE[0]}")"]}"
+  LD_PRELOAD="${ar18_old_ld_preload_map["${exit_script_path}"]}"
   # Restore old shell values
-  IFS=$'\n' shell_options=(echo ${ar18_old_shopt_map["$(realpath "${BASH_SOURCE[0]}")"]})
+  IFS=$'\n' shell_options=(echo ${ar18_old_shopt_map["${exit_script_path}"]})
   for option in "${shell_options[@]}"; do
     eval "${option}"
   done
